ecea83335cf83f45d6e9c7191f43044e0d29e099
68Kamil68 <tret1lus@gmail.com>
2020-05-23 01:47:41 +0200

    setup project

---
+++ Concentration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Concentration
+//
+//  Created by Kamil Krakowski on 23/05/2020.
+//  Copyright © 2020 Kamil Krakowski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
